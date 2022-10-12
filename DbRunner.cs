4301295e7b28d34ce085742d69c0a46eba00999a
Amir Hakim <50393714+amiroo4@users.noreply.github.com>
2022-10-12 12:41:45 +0200

    dbrunner and plugin button creation

---
+++ DbRunner.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Collections;
+
+namespace cityjsonToRevit
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    class DbRunner : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+
+            using (Transaction trans = new Transaction(doc, "Importer"))
+            {
+                trans.Start();
+
+                trans.Commit();
+
+                return Result.Succeeded;
+            }
+        }
+    }
+}
