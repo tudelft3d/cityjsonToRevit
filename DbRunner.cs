465f338d0407fe96090c4428a3bbe7fa99c93baf
Amir Hakim <50393714+amiroo4@users.noreply.github.com>
2022-10-13 15:46:20 +0200

    open and read JSON file

--- DbRunner.cs
+++ DbRunner.cs
@@ -10,12 +10,28 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Collections;
+using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace cityjsonToRevit
 {
     [Transaction(TransactionMode.Manual)]
     class DbRunner : IExternalCommand
     {
+        static public bool CheckValidity(dynamic file)
+        {
+            if (file.CityObjects == null || file.type != "CityJSON" || file.version == null ||
+                file.transform == null || file.transform.scale == null || file.transform.translate == null ||
+                file.vertices == null)
+            {
+                return false;
+            }
+            else if (file.version != "1.1" && file.version != "1.0")
+            {
+                return false;
+            }
+            return true;
+        }
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
 
@@ -27,6 +43,38 @@ namespace cityjsonToRevit
             {
                 trans.Start();
 
+                var fileContent = string.Empty;
+                var filePath = string.Empty;
+
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Title = "Open CityJSON file";
+                    openFileDialog.InitialDirectory = "c:\\";
+                    openFileDialog.Filter = "JSON files (*.JSON)|*.JSON";
+                    openFileDialog.FilterIndex = 1;
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        //Get the path of specified file
+                        filePath = openFileDialog.FileName;
+
+                        //Read the contents of the file into a stream
+                        var fileStream = openFileDialog.OpenFile();
+
+                        using (StreamReader reader = new StreamReader(fileStream))
+                        {
+                            string json = reader.ReadToEnd();
+                            dynamic array = JsonConvert.DeserializeObject(json);
+                            foreach (var item in array)
+                            {
+                                Console.WriteLine("{0}", item.Name);
+                            }
+                        }
+                    }
+                }
+
+                //MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
                 trans.Commit();
 
                 return Result.Succeeded;
