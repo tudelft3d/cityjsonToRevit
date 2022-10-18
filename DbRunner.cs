b7a520f8ee0d9478c8ff1ac5ad32aba52045d347
Amir Hakim <50393714+amiroo4@users.noreply.github.com>
2022-10-18 15:08:48 +0200

    Revit Geometry creation
    Using Directshape and TessellatedFace

--- DbRunner.cs
+++ DbRunner.cs
@@ -12,6 +12,10 @@ using System.Threading;
 using System.Collections;
 using System.Windows.Forms;
 using Newtonsoft.Json;
+using Autodesk.Revit.Creation;
+using Document = Autodesk.Revit.DB.Document;
+using System.Xml.Linq;
+using Autodesk.Revit.DB.Architecture;
 
 namespace cityjsonToRevit
 {
@@ -31,6 +35,69 @@ namespace cityjsonToRevit
                 return false;
             }
             return true;
+        }
+        public void CreateTessellatedShape(Autodesk.Revit.DB.Document doc, ElementId materialId)
+        {
+            List<XYZ> loopVertices = new List<XYZ>(4);
+
+            TessellatedShapeBuilder builder = new TessellatedShapeBuilder();
+
+            builder.OpenConnectedFaceSet(true);
+            // create a pyramid with a square base 4' x 4' and 5' high
+            double length = 4.0;
+            double height = 5.0;
+
+            XYZ basePt1 = XYZ.Zero;
+            XYZ basePt2 = new XYZ(length, 0, 0);
+            XYZ basePt3 = new XYZ(length, length, 0);
+            XYZ basePt4 = new XYZ(0, length, 0);
+            XYZ apex = new XYZ(length / 2, length / 2, height);
+
+            loopVertices.Add(basePt1);
+            loopVertices.Add(basePt2);
+            loopVertices.Add(basePt3);
+            loopVertices.Add(basePt4);
+            builder.AddFace(new TessellatedFace(loopVertices, materialId));
+
+            loopVertices.Clear();
+            loopVertices.Add(basePt1);
+            loopVertices.Add(apex);
+            loopVertices.Add(basePt2);
+            builder.AddFace(new TessellatedFace(loopVertices, materialId));
+
+            loopVertices.Clear();
+            loopVertices.Add(basePt2);
+            loopVertices.Add(apex);
+            loopVertices.Add(basePt3);
+            builder.AddFace(new TessellatedFace(loopVertices, materialId));
+
+            loopVertices.Clear();
+            loopVertices.Add(basePt3);
+            loopVertices.Add(apex);
+            loopVertices.Add(basePt4);
+            builder.AddFace(new TessellatedFace(loopVertices, materialId));
+
+            loopVertices.Clear();
+            loopVertices.Add(basePt4);
+            loopVertices.Add(apex);
+            loopVertices.Add(basePt1);
+            builder.AddFace(new TessellatedFace(loopVertices, materialId));
+
+            builder.CloseConnectedFaceSet();
+            builder.Target = TessellatedShapeBuilderTarget.Solid;
+            builder.Fallback = TessellatedShapeBuilderFallback.Abort;
+            builder.Build();
+
+            TessellatedShapeBuilderResult result = builder.GetBuildResult();
+
+      
+
+                DirectShape ds = DirectShape.CreateElement(doc, new ElementId(BuiltInCategory.OST_GenericModel));
+                ds.ApplicationId = "Application id";
+                ds.ApplicationDataId = "Geometry object id";
+
+                ds.SetShape(result.GetGeometricalObjects());
+            
         }
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -73,6 +140,19 @@ namespace cityjsonToRevit
                         }
                     }
                 }
+                //Selecting Default Material for shape creation
+
+
+                FilteredElementCollector collector = new FilteredElementCollector(doc).OfClass(typeof(Material));
+
+                IEnumerable<Material> materialsEnum
+                  = collector.ToElements().Cast<Material>().Where(e => e.Name == "Default");
+                Material materialDef = materialsEnum.First();
+
+
+
+                CreateTessellatedShape(doc, materialDef.Id);
+
 
                 //MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
                 trans.Commit();
