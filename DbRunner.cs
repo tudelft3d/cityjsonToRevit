4f58b4808af8353f7a7a7d1f82b87337e8ae2139
Amir Hakim <50393714+amiroo4@users.noreply.github.com>
2022-11-03 09:57:53 +0100

    create materials

--- DbRunner.cs
+++ DbRunner.cs
@@ -19,6 +19,7 @@ using Autodesk.Revit.DB.Architecture;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using Newtonsoft.Json.Linq;
 using DotSpatial.Projections;
+using Autodesk.Revit.DB.Visual;
 
 namespace cityjsonToRevit
 {
@@ -30,17 +31,17 @@ namespace cityjsonToRevit
         {
             string espg = unchecked((string)cityJ.metadata.referenceSystem);
             int found = espg.LastIndexOf(":");
-            if(found == 0)
-            { 
-                found = espg.LastIndexOf("/") ;
+            if (found == 0)
+            {
+                found = espg.LastIndexOf("/");
             }
-            espg = espg.Substring(found+1);
+            espg = espg.Substring(found + 1);
             int espgNo = Int32.Parse(espg);
             return espgNo;
         }
         public List<double> ShowActiveProjectLocationUsage(Autodesk.Revit.DB.Document document)
         {
-            List <double> coord = new List<double>();
+            List<double> coord = new List<double>();
             // Get the project location handle 
             ProjectLocation projectLocation = document.ActiveProjectLocation;
 
@@ -78,10 +79,6 @@ namespace cityjsonToRevit
             prompt += "\n\t\t" + "TimeZone: " + site.TimeZone;
             coord.Add(xlonDeg);
             coord.Add(ylatDeg);
-
-           
-
-
             // Give the user some information
             TaskDialog.Show("Revit", prompt);
             return coord;
@@ -92,10 +89,10 @@ namespace cityjsonToRevit
             SiteLocation site = document.ActiveProjectLocation.GetSiteLocation();
             ProjectionInfo pStart = ProjectionInfo.FromEpsgCode(espgNum(cityJ));
             ProjectionInfo pEnd = ProjectionInfo.FromEpsgCode(4326);
-            double[] xy = { cityJ.transform.translate[0], cityJ.transform.translate[1]};
+            double[] xy = { cityJ.transform.translate[0], cityJ.transform.translate[1] };
             double[] z = { 0 };
             Reproject.ReprojectPoints(xy, z, pStart, pEnd, 0, 1);
-            site.Latitude= xy[1] * angleRatio;
+            site.Latitude = xy[1] * angleRatio;
             site.Longitude = xy[0] * angleRatio;
 
         }
@@ -120,7 +117,7 @@ namespace cityjsonToRevit
             foreach (var objects in cityJ.CityObjects)
             {
 
-                foreach(var obj in objects)
+                foreach (var obj in objects)
                 {
                     if (obj.geometry == null)
                     {
@@ -147,7 +144,7 @@ namespace cityjsonToRevit
                     level = loder._level;
                 }
                 return level;
-            }  
+            }
         }
         public void CreateTessellatedShape(Autodesk.Revit.DB.Document doc, ElementId materialId, dynamic cityObjProp, List<XYZ> verticesList, string Namer, string Lod)
         {
@@ -160,7 +157,7 @@ namespace cityjsonToRevit
             foreach (var boundaryGroup in cityObjProp.geometry)
             {
 
-                if (Lod == (string)boundaryGroup.lod) 
+                if (Lod == (string)boundaryGroup.lod)
                 {
                     builder.OpenConnectedFaceSet(false);
                     foreach (var boundary in boundaryGroup.boundaries)
@@ -215,7 +212,7 @@ namespace cityjsonToRevit
                     ds.SetShape(result.GetGeometricalObjects());
                 }
 
-            }            
+            }
         }
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -235,12 +232,6 @@ namespace cityjsonToRevit
             using (Transaction trans = new Transaction(doc, "Load CityJSON"))
             {
                 trans.Start();
-
-
-
-
-
-                
                 var fileContent = string.Empty;
                 var filePath = string.Empty;
                 List<double> coord = ShowActiveProjectLocationUsage(doc);
@@ -267,72 +258,128 @@ namespace cityjsonToRevit
                             dynamic jCity = JsonConvert.DeserializeObject(json);
                             ProjectionInfo pStart = ProjectionInfo.FromEpsgCode(espgNum(jCity));
                             ProjectionInfo pEnd = ProjectionInfo.FromEpsgCode(4326);
-                            const double angleRatio = Math.PI / 180;
+                            //const double angleRatio = Math.PI / 180;
                             List<XYZ> vertList = new List<XYZ>();
-                            bool newLocation = true;
-                            if (newLocation)
+                            //bool newLocation = true;
+                            //if (newLocation)
+                            //{
+                            foreach (var vertex in jCity.vertices)
                             {
-                                foreach (var vertex in jCity.vertices)
-                                {
-                                    double x = vertex[0] * jCity.transform.scale[0];
-                                    //+ jCity.transform.translate[0] - coord[0];
-                                    double y = vertex[1] * jCity.transform.scale[1];
-                                    double z = vertex[2] * jCity.transform.scale[2];
-                                    double xx = UnitUtils.ConvertToInternalUnits(x, UnitTypeId.Meters);
-                                    double yy = UnitUtils.ConvertToInternalUnits(y, UnitTypeId.Meters);
-                                }
-                                UpdateSiteLocation(doc, jCity);
+                                double x = vertex[0] * jCity.transform.scale[0];
+                                //+ jCity.transform.translate[0] - coord[0];
+                                double y = vertex[1] * jCity.transform.scale[1];
+                                double z = vertex[2] * jCity.transform.scale[2];
+                                double xx = UnitUtils.ConvertToInternalUnits(x, UnitTypeId.Meters);
+                                double yy = UnitUtils.ConvertToInternalUnits(y, UnitTypeId.Meters);
+                                double zz = UnitUtils.ConvertToInternalUnits(z, UnitTypeId.Meters);
+                                XYZ vert = new XYZ(xx, yy, zz);
+                                vertList.Add(vert);
                             }
-                            else
-                            {
-                                foreach (var vertex in jCity.vertices)
-                                {
-                                    double x = vertex[0] * jCity.transform.scale[0] + jCity.transform.translate[0];
-                                    double y = vertex[1] * jCity.transform.scale[1] + jCity.transform.translate[1];
-                                    double z = vertex[2] * jCity.transform.scale[2] + jCity.transform.translate[2];
-                                    double[] xy = { x, y };
-                                    double[] zy = { 0 };
-                                    Reproject.ReprojectPoints(xy, zy, pStart, pEnd, 0, 1);
-                                    SiteLocation site = doc.ActiveProjectLocation.GetSiteLocation();
-                                    ;
-                                    double xDeg = xy[0] - site.Longitude / angleRatio;
-                                    double yDeg = xy[1] - site.Latitude / angleRatio;
+                            UpdateSiteLocation(doc, jCity);
+                            //}
+                            //else
+                            //{
+                            //    foreach (var vertex in jCity.vertices)
+                            //    {
+                            //        double x = vertex[0] * jCity.transform.scale[0] + jCity.transform.translate[0];
+                            //        double y = vertex[1] * jCity.transform.scale[1] + jCity.transform.translate[1];
+                            //        double z = vertex[2] * jCity.transform.scale[2] + jCity.transform.translate[2];
+                            //        double[] xy = { x, y };
+                            //        double[] zy = { 0 };
+                            //        Reproject.ReprojectPoints(xy, zy, pStart, pEnd, 0, 1);
+                            //        SiteLocation site = doc.ActiveProjectLocation.GetSiteLocation();
+                            //        ;
+                            //        double xDeg = xy[0] - site.Longitude / angleRatio;
+                            //        double yDeg = xy[1] - site.Latitude / angleRatio;
 
-                                    double xx = UnitUtils.ConvertToInternalUnits(xy[0] * (10000 * 1000 / 90), UnitTypeId.Meters);
-                                    double yy = UnitUtils.ConvertToInternalUnits(xy[1] * (10000 * 1000 / 90), UnitTypeId.Meters);
-                                    double zz = UnitUtils.ConvertToInternalUnits(z, UnitTypeId.Meters);
-                                    XYZ vert = new XYZ(xx, yy, zz);
-                                    vertList.Add(vert);
-                                }
-                            }
-                            
-                            //UpdateSiteLocation(doc, jCity);
+                            //        double xx = UnitUtils.ConvertToInternalUnits(xy[0] * (10000 * 1000 / 90), UnitTypeId.Meters);
+                            //        double yy = UnitUtils.ConvertToInternalUnits(xy[1] * (10000 * 1000 / 90), UnitTypeId.Meters);
+                            //        double zz = UnitUtils.ConvertToInternalUnits(z, UnitTypeId.Meters);
+                            //        XYZ vert = new XYZ(xx, yy, zz);
+                            //        vertList.Add(vert);
+                            //    }
+                            //}
+
                             string lodSpec = lodSelecter(jCity);
                             foreach (var objects in jCity.CityObjects)
                             {
-                                foreach(var objProperties in  objects)
+                                foreach (var objProperties in objects)
                                 {
-                                        string attributeName = objects.Name;
-                                        CreateTessellatedShape(doc, materialDef.Id, objProperties, vertList, attributeName, lodSpec);
-                                } 
+                                    string attributeName = objects.Name;
+                                    CreateTessellatedShape(doc, materialDef.Id, objProperties, vertList, attributeName, lodSpec);
+                                }
                             }
-                                TaskDialog.Show("Good!", "All set! Let's Go!\n");
+                            TaskDialog.Show("Good!", "All set! Let's Go!\n");
                         }
                     }
                 }
-                Material cj00 = materialDef.Duplicate("cj-Building");
-                Material cj01 = materialDef.Duplicate("cj-Bridge");
-                Material cj02 = materialDef.Duplicate("cj-Group");
-                Material cj03 = materialDef.Duplicate("cj-Furniture");
-                Material cj04 = materialDef.Duplicate("cj-Landuse");
-                Material cj05 = materialDef.Duplicate("cj-Plants");
-                Material cj06 = materialDef.Duplicate("cj-Railway");
-                Material cj07 = materialDef.Duplicate("cj-Road");
-                Material cj08 = materialDef.Duplicate("cj-Tunnel");
-                Material cj09 = materialDef.Duplicate("cj-Water");
+
+
                 trans.Commit();
-                return Result.Succeeded;
+
             }
+
+            IEnumerable<Material> checkMat
+              = collector.ToElements().Cast<Material>().Where(e => e.Name == "cj-Building");
+
+            if (!checkMat.Any())
+            {
+                using (Transaction t = new Transaction(doc, "Set CJ Materials"))
+                {
+                    t.Start();
+                    List<Material> mats = new List<Material>();
+                    Material cj00 = materialDef.Duplicate("cj-Building");
+                    cj00.Color = new Color(119, 136, 153);
+                    mats.Add(cj00);
+                    Material cj01 = materialDef.Duplicate("cj-Bridge");
+                    cj01.Color = new Color(160, 82, 45);
+                    mats.Add(cj01);
+                    Material cj02 = materialDef.Duplicate("cj-Group");
+                    cj02.Color = new Color(218, 165, 32);
+                    mats.Add(cj02);
+                    Material cj03 = materialDef.Duplicate("cj-Furniture");
+                    cj03.Color = new Color(255, 69, 0);
+                    mats.Add(cj03);
+                    Material cj04 = materialDef.Duplicate("cj-Landuse");
+                    cj04.Color = new Color(218, 165, 32);
+                    mats.Add(cj04);
+                    Material cj05 = materialDef.Duplicate("cj-Plants");
+                    cj05.Color = new Color(0, 204, 0);
+                    mats.Add(cj05);
+                    Material cj06 = materialDef.Duplicate("cj-Railway");
+                    cj06.Color = new Color(0, 0, 0);
+                    mats.Add(cj06);
+                    Material cj07 = materialDef.Duplicate("cj-Road");
+                    cj07.Color = new Color(64, 64, 64);
+                    mats.Add(cj07);
+                    Material cj08 = materialDef.Duplicate("cj-Tunnel");
+                    cj08.Color = new Color(51, 25, 0);
+                    mats.Add(cj08);
+                    Material cj09 = materialDef.Duplicate("cj-Water");
+                    cj09.Color = new Color(0, 128, 255);
+                    mats.Add(cj09);
+
+                    foreach (Material m in mats)
+                    {
+                        ElementId appearanceAssetId = m.AppearanceAssetId;
+                        AppearanceAssetElement assetElem = m.Document.GetElement(appearanceAssetId) as AppearanceAssetElement;
+                        ElementId duplicateAssetElementId = ElementId.InvalidElementId;
+                        AppearanceAssetElement duplicateAssetElement = assetElem.Duplicate(m.Name);
+                        m.AppearanceAssetId = duplicateAssetElement.Id;
+                        duplicateAssetElementId = duplicateAssetElement.Id;
+                        using (AppearanceAssetEditScope editScope = new AppearanceAssetEditScope(assetElem.Document))
+                        {
+                            Asset editableAsset = editScope.Start(duplicateAssetElementId);
+                            AssetPropertyDoubleArray4d genericDiffuseProperty = editableAsset.FindByName("generic_diffuse") as AssetPropertyDoubleArray4d;
+                            genericDiffuseProperty.SetValueAsColor(m.Color);
+                            editScope.Commit(true);
+                        }
+                    }
+                    t.Commit();
+                }
+            }
+            
+            return Result.Succeeded;
         }
     }
 }
