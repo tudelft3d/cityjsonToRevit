06e5b2af9780baabc55785930739509d94120f47
Amir Hakim <50393714+amiroo4@users.noreply.github.com>
2022-12-14 16:54:50 +0100

    create hideUnhide

--- Command/ExternalApplication.cs
+++ Command/ExternalApplication.cs
@@ -17,12 +17,13 @@ namespace cityjsonToRevit
         public Result OnStartup(UIControlledApplication application)
         {
             //create Ribbon Tab
-            application.CreateRibbonTab("TU Delft");
+            application.CreateRibbonTab("3D geoinfo");
             string path = Assembly.GetExecutingAssembly().Location;
 
 
-            PushButtonData button1 = new PushButtonData("Button1", "Import CityJSON", path, "cityjsonToRevit.Program");
-            RibbonPanel panel = application.CreateRibbonPanel("TU Delft", "CityJSON");
+            PushButtonData button1 = new PushButtonData("Button1", "Import", path, "cityjsonToRevit.Program");
+            PushButtonData button2 = new PushButtonData("Button2", "Hide/Unhide", path, "cityjsonToRevit.Hide");
+            RibbonPanel panel = application.CreateRibbonPanel("3D geoinfo", "CityJSON");
 
             // ExternalCommands assembly path
             string AddInPath = typeof(ExternalApplication).Assembly.Location;
@@ -32,16 +33,22 @@ namespace cityjsonToRevit
             Uri imagepath1 = new Uri(Path.Combine(ButtonIconsFolder, "images/3dgeo.png"), UriKind.Absolute);
             BitmapImage image1 = new BitmapImage(imagepath1);
 
+            Uri imagepath2 = new Uri(Path.Combine(ButtonIconsFolder, "images/eye.png"), UriKind.Absolute);
+            BitmapImage image2 = new BitmapImage(imagepath2);
+
             PushButton pushButton1 = panel.AddItem(button1) as PushButton;
             pushButton1.LargeImage = image1;
 
+            PushButton pushButton2 = panel.AddItem(button2) as PushButton;
+            pushButton2.LargeImage = image2;
+
             pushButton1.ToolTip = "Import geometries and attributes from a CityJSON file";
-
             pushButton1.LongDescription = "Specify the CityJSON file that you want to import. Choose whether to keep or update Revit Origin. In the event that there are multiple LODs, identify the level you desire to be generated.";
-
             ContextualHelp contextHelp = new ContextualHelp(ContextualHelpType.Url, "https://apps.autodesk.com/RVT/en/Detail/HelpDoc?appId=7787623024858844510&appLang=en&os=Win64&mode=preview");
             pushButton1.SetContextualHelp(contextHelp);
 
+            pushButton2.ToolTip = "Hide/Unhide imported elements in the active view";
+            pushButton2.SetContextualHelp(contextHelp);
 
 
             return Result.Succeeded;
---
+++ Command/Hide.cs
@@ -0,0 +1,60 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Collections;
+
+namespace cityjsonToRevit
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    class Hide : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            Autodesk.Revit.DB.View actView = doc.ActiveView;
+
+
+            using (Transaction trans = new Transaction(doc, "Hide Unhide CJ"))
+            {
+                trans.Start();
+                ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_GenericModel);
+                FilteredElementCollector collector = new FilteredElementCollector(doc);
+                ICollection<ElementId> elemIds = collector.WherePasses(filter).WhereElementIsNotElementType().ToElementIds();
+                List<ElementId> hlist = new List<ElementId>();
+                List<ElementId> uhlist = new List<ElementId>();
+
+                foreach (ElementId eid in elemIds)
+                {
+                    if (doc.GetElement(eid).IsHidden(actView))
+                    {
+                        uhlist.Add(eid);
+                    }
+                    else
+                    {
+                        hlist.Add(eid);
+                    }
+                }
+                ICollection<ElementId> uhcol = uhlist;
+                ICollection<ElementId> hcol = hlist;
+
+                if(hcol.Count!= 0)
+                actView.HideElements(hcol);
+                if (uhcol.Count != 0)
+                actView.UnhideElements(uhcol);
+
+                trans.Commit();
+
+                return Result.Succeeded;
+            }
+        }
+    }
+}
