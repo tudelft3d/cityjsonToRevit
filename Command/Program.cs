2bd6fb08b7065d5e0d173322d4d4543b532c7920
Amir Hakim <50393714+amiroo4@users.noreply.github.com>
2023-01-23 15:32:19 +0100

    optimize mat select and generate

--- Command/Program.cs
+++ Command/Program.cs
@@ -232,10 +232,10 @@ namespace cityjsonToRevit
 
             }
         }
-        public static List<Material> matGenerator(Document doc)
-         {
-            FilteredElementCollector collector = new FilteredElementCollector(doc).OfClass(typeof(Material));
 
+        public static List<Material> matGenerator(Document doc, FilteredElementCollector collector)
+
+        {
             IEnumerable<Material> existingMats
               = collector.ToElements().Cast<Material>();
             List<Material> mats = new List<Material>();
@@ -296,9 +296,10 @@ namespace cityjsonToRevit
             return mats;
         }
 
-        public static Material matSelector(List<Material> materials, string type, Document doc)
+
+        public static Material matSelector(List<Material> materials, string type, FilteredElementCollector collector)
+
         {
-            FilteredElementCollector collector = new FilteredElementCollector(doc).OfClass(typeof(Material));
             Material m
               = collector.ToElements().Cast<Material>().Where(e => e.Name == "cj-Default").First();
 
@@ -420,9 +421,9 @@ namespace cityjsonToRevit
                 //Get the path of specified file
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
-
-                    List<Material> materials = matGenerator(doc);
-
+                    FilteredElementCollector matcollector = new FilteredElementCollector(doc).OfClass(typeof(Material));
+                    List<Material> materials = matGenerator(doc , matcollector);
+                    matcollector = new FilteredElementCollector(doc).OfClass(typeof(Material));
 
                     using (Transaction trans = new Transaction(doc, "Load CityJSON"))
                     {
@@ -543,7 +544,7 @@ namespace cityjsonToRevit
                                 {
                                     string attributeName = objects.Name;
                                     string objType = unchecked((string)objProperties.type);
-                                    Material mat = matSelector(materials, objType, doc);
+                                    Material mat = matSelector(materials, objType, matcollector);
                                     CreateTessellatedShape(doc, mat.Id, objProperties, vertList, attributeName, lodSpec, paramets, semanticParentInfo);
                                 }
                             }
