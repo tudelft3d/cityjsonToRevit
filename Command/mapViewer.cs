0f0c794d56d7eeb646e9092c824460db0071b1e1
Amir Hakim <50393714+amiroo4@users.noreply.github.com>
2023-01-24 23:02:56 +0100

    Handle mapview close event

--- Command/mapViewer.cs
+++ Command/mapViewer.cs
@@ -6,28 +6,30 @@ namespace cityjsonToRevit
     public partial class mapViewer : Form
     {
         public bool _loc = false;
+        public bool _cancel = true;
+
         public mapViewer(double lat0, double lon0, double lat1, double lon1)
         {
             InitializeComponent(lat0, lon0, lat1, lon1);
         }
 
-        public bool NewLoc(bool loca)
+
+        public bool NewCan(bool loca)
         {
-            _loc = loca;
-            return _loc;
+            _cancel = loca;
+            return _cancel;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            bool loc = false;
-            NewLoc(loc);
+            _cancel = false;
             this.Close();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            bool loc = true;
-            NewLoc(loc);
+            _cancel = false;
+            _loc = true;
             this.Close();
         }
     }
